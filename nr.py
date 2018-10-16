09f0c7a76f8c06b366fc2add9d63eec1c264a42c
NamArkal <namrataap@gmail.com>
2018-10-15 21:10:05 -0400

    Modifying branching factor

--- nr.py
+++ nr.py
@@ -1,50 +1,19 @@
-import matplotlib.pyplot as plt
-
-N = 17
-d = 3
-
-
-def set_vals(n, D):
-    N = n
-    d = D
-
-
-# 1 + 2 + 2*2 + 2**3 = 14+1
-
-# (b**(d+1) - 1) / (b - 1) ~ b**(d+1) / b = b**d
-# b**d ~ N+1
-# b ~ (N+1)**(1./d)
-
-# print(N, d)
-
-
-def g(b):
+def g(b, n, d):
     mysum = 1
     for i in range(1, d + 1):
         mysum += b ** i
-    return mysum - (N + 1)
+    return mysum - (n + 1)
 
 
-def dg(b):
+def dg(b, d):
     mysum = 1
     for i in range(1, d):
         mysum += (i + 1) * b ** i
     return mysum
 
 
-def newton(b0, g, dg):
+def newton(b0, g, dg, nc, d):
     b = [b0]
     for n in range(0, 10):
-        print(b[n])
-        b.append(b[n] - g(b[n]) / dg(b[n]))
+        b.append(b[n] - g(b[n], nc, d) / dg(b[n], d))
     return b
-
-
-b0 = (N + 1) ** (1. / d)
-b = newton(b0, g, dg)
-
-# print("Check:")
-print(b[-1])
-
-# plt.plot(range(len(b)), map(g, b))
-# plt.show()
